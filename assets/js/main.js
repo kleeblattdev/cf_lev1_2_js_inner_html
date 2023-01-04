e63a0dd4002a515554d8729bc6820d96015158eb
karinlee <karinlee92@gmail.com>
2023-01-04 12:39:45 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let info = document.getElementById("info");
+
+info.innerHTML += "<h1>Hello World!</h1>";
+console.log(info);
+
+info.innerHTML += "<h2> How are you?</h2>";
+console.log(info);
+
+let container = document.getElementById("container");
+
+container.innerHTML += "<p>start of the element</p>";
+
+document.write("end of the element");
